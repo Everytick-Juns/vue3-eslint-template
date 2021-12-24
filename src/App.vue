91046aec19cbdd82f3189bfcfbc1f484793ec7b2
Junseong <6_5ee@naver.com>
2021-12-24 11:23:02 +0900

    인스턴스와 라이프사이클

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,27 @@
 <template>
-  <h1 @click="increase">
-    {{ count }}
-  </h1>
-  <div v-if="count > 4">
-    4보다 큽니다!
-  </div>
-  <ul>
-    <Fruit
-      v-for="fruit in fruits"
-      :key="fruit"
-      :name="fruit">
-      {{ fruit }}
-    </Fruit>
-  </ul>
+  <h1>{{ count }}</h1>
 </template>
 
-
 <script>
-import Fruit from '~/components/Fruit'
-
-
-export default  {
-  components: {
-    Fruit
-  },
-  
-  data() {
-    return {
-      count: 0,
-      fruits: ['Apple', 'Banana', 'Cherry']
-    }
-  },
-  methods: {
-    increase() {
-      this.count += 1
-    }
+  export default {
+    data() {
+      return {
+        count: 2
+      }
+    },
+    beforeCreate(){// 데이터가 생성되기 전 가장 처음으로 실행됨.
+      console.log('beforeCreated!', this.count)
+    },
+    created(){// 데이터가 생성된 이후, html과 연결되기 전, 활용도 높고 중요
+      console.log('Created!', this.count)
+    },
+    beforeMount(){ // html과 연결 되기 전 - 활용도 떨어짐.
+      console.log('beforeCreated!')
+      console.log(document.querySelector('h1'))
+    },
+    mounted(){// html과 연결 된 후 , 활용도 높고 중요
+      console.log("Mounted!")
+      console.log(document.querySelector('h1'))
+    },
   }
-}
 </script>
-
-<style lang="scss">
-  h1{
-    font-size: 50px;
-    color: royalblue;
-  }
-  ul{
-    li{
-      font-size: 40px;
-
-    }
-  }
-</style>
