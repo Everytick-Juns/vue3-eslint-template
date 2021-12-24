2b95f8e0286196c666a75ddcefc21d8cbdf414db
Junseong <6_5ee@naver.com>
2021-12-24 11:44:58 +0900

    README.ad 참고(기본문법)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vue 문법
+a# Vue 문법
 - html 태그 안에 @click="함수명"  <해당 태그 클릭시 실행>
 - 태그 안에 v-if="조건식"
 - v-for="fruit in fruits" :key="fruit" :name="fruit" {{ fruit }}
@@ -10,3 +10,39 @@
 
 - created
 - mounted
+
+## Vue 보간법(Interpolation)
+- 문자열 : 데이터 바인딩의 가장 기본 형태는 "Mustache"(이중 중괄호 구문) 기법을 사용한 문자열 보간법(text interpolation)
+- v-once 디렉티브를 사용하여 데이터가 변경되어도 갱신되지 않는 일회성 보간을 사용할 수 있다.
+
+## v-html (원시 html)
+```vue
+<h1 v-html="msg"></h1>
+```
+- 해당 부분은 원시 html로 인식할 수 있다.
+
+## v-bind
+- html 속성에서 사용할 수 없는 부분은 바인드를 통해서 연결할 수 있다.
+```vue
+// 전체 문법
+<h1 v-bind:id="msg"></h1>
+
+//약어
+<a :href="url"></a>
+
+// 동적 전달인자와 함께 쓴 약어
+<a :[key]="url">
+```
+
+## v-on
+```vue
+<a v-on:click="doSomething></a>
+
+//약어
+<a @click="doSomething></a>
+
+//동적 전달인자와 함께 쓴 약어
+<a @[event]="doSomething"> </a>
+
+```
+
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <h1>{{ count }}</h1>
+  <h1
+    :[attr]="'active'"
+    @[event]="add">
+    {{ msg }}
+  </h1>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        count: 2
-      }
-    },
-    beforeCreate(){// 데이터가 생성되기 전 가장 처음으로 실행됨.
-      console.log('beforeCreated!', this.count)
-    },
-    created(){// 데이터가 생성된 이후, html과 연결되기 전, 활용도 높고 중요
-      console.log('Created!', this.count)
-    },
-    beforeMount(){ // html과 연결 되기 전 - 활용도 떨어짐.
-      console.log('beforeCreated!')
-      console.log(document.querySelector('h1'))
-    },
-    mounted(){// html과 연결 된 후 , 활용도 높고 중요
-      console.log("Mounted!")
-      console.log(document.querySelector('h1'))
-    },
-  }
+ export default{
+   data(){
+     return {
+       msg: 'active',
+       attr: 'class',
+       event: 'click'
+     }
+   },
+   methods: {
+     add(){
+       this.msg += '!'
+     }
+   }
+ }
 </script>
+
+<style scoped lang="scss">
+  .active{
+    color: royalblue;
+    font-size: 100px;
+  }
+
+</style>
